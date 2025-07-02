b7793dc85bfb46dd47480712cb8a2261f3ed1532
Zdeněk <zdenektatakjr@proton.me>
2025-07-02 14:03:57 +0200

    Update __.init.__.py

--- app/__.init.__.py
+++ app/__.init.__.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
 
+def create_app():
+    load_dotenv()  # loads the API key from '.env'
+    app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = 'app/uploads'
+    
+    from app.routes import main
+    app.register_blueprint(main)
+    
+    return app
